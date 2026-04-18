b732d48291d893cb3d4a10534b59d5e5d5ecd04a
Javier Mendez <88914048+JaviMendez88@users.noreply.github.com>
2026-04-18 12:24:44 -0600

    Creacion Vistas, Modelos, Controllers
    Creación de Layouts, Vistas, Modelos, Controllers.

---
+++ CasoEstudioKN/Models/AlquileresModelo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CasoEstudioKN.Models
+{
+    public class AlquileresModelo
+    {
+        [Required]
+        public long IdCasa { get; set; }
+
+        public decimal PrecioCasa { get; set; }
+
+        [Required(ErrorMessage = "El usuario es obligatorio")]
+        [StringLength(30)]
+        public string UsuarioAlquiler { get; set; }
+
+    }
+}
---
+++ CasoEstudioKN/Models/ConsultasModelo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CasoEstudioKN.Models
+{
+    public class ConsultasModelo
+    {
+        public long IdCasa { get; set; }
+        public string DescripcionCasa { get; set; }
+        public decimal PrecioCasa { get; set; }
+        public string UsuarioAlquiler { get; set; }
+        public DateTime? FechaAlquiler { get; set; }
+
+        public string Estado
+        {
+            get
+            {
+                return string.IsNullOrEmpty(UsuarioAlquiler)
+                    ? "Disponible"
+                    : "Reservada";
+            }
+        }
+
+    }
+}
